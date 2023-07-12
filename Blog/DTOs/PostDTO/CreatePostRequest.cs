33b3da244cd822d6bb4ce794f04bc2b25076adbf
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-07-12 12:35:53 +0200

    prettier changes

--- Blog/DTOs/PostDTO/CreatePostRequest.cs
+++ Blog/DTOs/PostDTO/CreatePostRequest.cs
@@ -4,7 +4,6 @@ namespace Blog.DTOs.PostDTO
 {
     public class CreatePostRequest
     {
-
         [Required(ErrorMessage = "Title is required")]
         [StringLength(100, ErrorMessage = "Title must be between 1 and 100 characters", MinimumLength = 1)]
         public string Title { get; set; } = string.Empty;
